a1548ca97568f416ab0f29852b3525fdba2f15e6
Om Songire <160092010+Omsongire23@users.noreply.github.com>
2025-06-28 17:46:51 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the First Number : "))
+b=int(input("Enter the Second Number : "))
+print("Addition of the Two Number is ",a+b)
+print("Subtraction of the Two Number is ",a-b)
+print("Multiplication of the Two Number is ",a*b)
+print("Division of the Two Number is ",a/b)
+
---
+++ Task2.py
@@ -0,0 +1,3 @@
+a = input("Enter the First Name : ")
+b = input("Enter the Last Name : ")
+print("Hello, " + a +" " + b + "! Welcome to the Python Program.")
